44de5968381e6f0a950ffe7826ab6cd594b010b6
Damien Butt <damien.butt@norgateav.com>
2022-04-05 05:08:24 +0100

    chore(digital): split digitals into separate event and state enums

---
+++ src/CrestronCH5/enums/Digital/State/index.ts
@@ -0,0 +1,6 @@
+export enum DigitalState {
+	Csig_All_Control_Systems_Online_fb = "Csig.All_Control_Systems_Online_fb",
+	Csig_Control_Systems_Offline_fb = "Csig.Control_Systems_Offline_fb",
+}
+
+export default DigitalState;
