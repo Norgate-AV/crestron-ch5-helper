5782d99eb04b527e78d8831411cfa64308a2f93d
Damien Butt <damien.butt@norgateav.com>
2022-04-06 02:57:58 +0100

    chore(digitals): add some more digital state constants

--- src/CrestronCH5/enums/Digital/State/index.ts
+++ src/CrestronCH5/enums/Digital/State/index.ts
@@ -1,6 +1,91 @@
 export enum DigitalState {
+	Csig_24_Hr_Clock_Off_fb = "Csig.24_Hr_Clock_Off_fb",
+	Csig_24_Hr_Clock_On_fb = "Csig.24_Hr_Clock_On_fb",
+
+	Csig_Admin_fb = "Csig.Admin_fb",
+
+	Csig_All_Audio_Off_fb = "Csig.All_Audio_Off_fb",
+	Csig_All_Audio_On_fb = "Csig.All_Audio_On_fb",
+
 	Csig_All_Control_Systems_Online_fb = "Csig.All_Control_Systems_Online_fb",
+
+	Csig_Authenticated_fb = "Csig.Authenticated_fb",
+	Csig_Authentication_On_fb = "Csig.Authentication_On_fb",
+	Csig_Auto_Answer_fb = "Csig.Auto_Answer_fb",
+
+	Csig_Backlight_Off_fb = "Csig.Backlight_Off_fb",
+	Csig_Backlight_On_fb = "Csig.Backlight_On_fb",
+
+	Csig_BlueLedControl_fb = "Csig.BlueLedControl_fb",
+	Csig_BlueStatusBarControl_fb = "Csig.BlueStatusBarControl_fb",
+
+	Csig_Browser_Kiosk_Off_fb = "Csig.Browser_kiosk_off_fb",
+	Csig_Browser_Kiosk_On_fb = "Csig.Browser_kiosk_on_fb",
+	Csig_Busy_fb = "Csig.Busy_fb",
+	Csig_Button_1_Off_fb = "Csig.Button_1_Off_fb",
+	Csig_Button_2_Off_fb = "Csig.Button_2_Off_fb",
+	Csig_Button_3_Off_fb = "Csig.Button_3_Off_fb",
+	Csig_Button_4_Off_fb = "Csig.Button_4_Off_fb",
+	Csig_Button_5_Off_fb = "Csig.Button_5_Off_fb",
+	Csig_Button_6_Off_fb = "Csig.Button_6_Off_fb",
+	Csig_Button_1_On_fb = "Csig.Button_1_On_fb",
+	Csig_Button_2_On_fb = "Csig.Button_2_On_fb",
+	Csig_Button_3_On_fb = "Csig.Button_3_On_fb",
+	Csig_Button_4_On_fb = "Csig.Button_4_On_fb",
+	Csig_Button_5_On_fb = "Csig.Button_5_On_fb",
+	Csig_Button_6_On_fb = "Csig.Button_6_On_fb",
+	Csig_Button_Toolbar_Disable_fb = "Csig.Button_Toolbar_Disable_fb",
+	Csig_Button_Toolbar_Enable_fb = "Csig.Button_Toolbar_Enable_fb",
+	Csig_Call_Terminated_fb = "Csig.Call_Terminated_fb",
+	Csig_Call_Active_fb = "Csig.Call_Active_fb",
+
+	Csig_Close_Browser_fb = "Csig.Close_Browser_fb",
+
+	Csig_ConnectedToServer_fb = "Csig.ConnectedToServer_fb",
 	Csig_Control_Systems_Offline_fb = "Csig.Control_Systems_Offline_fb",
+
+	Csig_Dialing_fb = "Csig.Dialing_fb",
+	Csig_Disable_fb = "Csig.Disable_fb",
+	Csig_DisableMulticast_fb = "Csig.DisableMulticast_fb",
+	Csig_DisableStream_fb = "Csig.DisableStream_fb",
+	Csig_Do_Not_Disturb_fb = "Csig.Do_Not_Disturb_fb",
+
+	Csig_Enable_fb = "Csig.Enable_fb",
+	Csig_EnableMulticast_fb = "Csig.EnableMulticast_fb",
+	Csig_EnableStream_fb = "Csig.EnableStream_fb",
+
+	Csig_GreenLedControl_fb = "Csig.GreenLedControl_fb",
+	Csig_GreenStatusBarControl_fb = "Csig.GreenStatusBarControl_fb",
+
+	Csig_Hard_Button_1_Off_fb = "Csig.Hard_Button_1_Off_fb",
+	Csig_Hard_Button_2_Off_fb = "Csig.Hard_Button_2_Off_fb",
+	Csig_Hard_Button_3_Off_fb = "Csig.Hard_Button_3_Off_fb",
+	Csig_Hard_Button_4_Off_fb = "Csig.Hard_Button_4_Off_fb",
+	Csig_Hard_Button_5_Off_fb = "Csig.Hard_Button_5_Off_fb",
+	Csig_Hard_Button_1_On_fb = "Csig.Hard_Button_1_On_fb",
+	Csig_Hard_Button_2_On_fb = "Csig.Hard_Button_2_On_fb",
+	Csig_Hard_Button_3_On_fb = "Csig.Hard_Button_3_On_fb",
+	Csig_Hard_Button_4_On_fb = "Csig.Hard_Button_4_On_fb",
+	Csig_Hard_Button_5_On_fb = "Csig.Hard_Button_5_On_fb",
+	Csig_Hard_Button_Brt_Auto_Off_fb = "Csig.Hard_Button_Brt_Auto_Off_fb",
+	Csig_Hard_Button_Brt_Auto_On_fb = "Csig.Hard_Button_Brt_Auto_On_fb",
+
+	Csig_Hide_Button_Toolbar_fb = "Csig.Hide_Button_Toolbar_fb",
+	Csig_Hide_Opened_App_fb = "Csig.Hide_Opened_App_fb",
+	Csig_Hold_fb = "Csig.Hold_fb",
+	Csig_In_Progress_fb = "Csig.In_Progress_fb",
+	Csig_Incoming_fb = "Csig.Incoming_fb",
+	Csig_Key_Backlight_High_Level_fb = "Csig.Key_Backlight_High_Level_fb",
+	Csig_Key_Backlight_Low_Level_fb = "Csig.Key_Backlight_Low_Level_fb",
+
+	Csig_Key_Backlight_Off_fb = "Csig.Key_Backlight_Off_fb",
+	Csig_Key_Backlight_On_fb = "Csig.Key_Backlight_On_fb",
+	Csig_Key_Click_Off_fb = "Csig.Key_Click_Off_fb",
+	Csig_Key_Click_On_fb = "Csig.Key_Click_On_fb",
+
+	Csig_Landscape_Orientation_fb = "Csig.Landscape_Orientation_fb",
+	Csig_Lcd_Brt_Auto_Off_fb = "Csig.Lcd_Brt_Auto_Off_fb",
+	Csig_Lcd_Brt_Auto_On_fb = "Csig.Lcd_Brt_Auto_On_fb",
 }
 
 export default DigitalState;
