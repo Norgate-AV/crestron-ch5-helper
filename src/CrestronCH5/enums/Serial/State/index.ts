44b29d36ee8734a40983ba30463bcee9eacc37b7
Damien Butt <damien.butt@norgateav.com>
2022-04-07 00:41:41 +0100

    chore(serial): add serial state constants

--- src/CrestronCH5/enums/Serial/State/index.ts
+++ src/CrestronCH5/enums/Serial/State/index.ts
@@ -1,3 +1,26 @@
-export enum SerialState {}
+export enum SerialState {
+	Csig_Admin_Account_Username_fb = "Csig.Admin_Account_Username_fb",
+
+	Csig_Browser_Homepage_fb = "Csig.Browser_Homepage_fb",
+
+	Csig_CameraSnapshotName_fb = "Csig.CameraSnapshotName_fb",
+	Csig_CameraSnapshotURL_fb = "Csig.CameraSnapshotURL_fb",
+	Csig_CameraStreamName_fb = "Csig.CameraStreamName_fb",
+	Csig_CameraStreamURL_fb = "Csig.CameraStreamURL_fb",
+	Csig_Check_Time_fb = "Csig.Check_Time_fb",
+	Csig_DialBuffer_fb = "Csig.DialBuffer_fb",
+	Csig_Ip_Address_fb = "Csig.Ip_Address_fb",
+	Csig_MAC_Address_fb = "Csig.MAC_Address_fb",
+	Csig_Manifest_URL_fb = "Csig.Manifest_URL_fb",
+	Csig_MulticastAddress_fb = "Csig.MulticastAddress_fb",
+	Csig_Room_Name_fb = "Csig.Room_Name_fb",
+	Csig_Screensaver_Image_URL_fb = "Csig.Screensaver_Image_URL_fb",
+	Csig_Screensaver_Logo_URL_fb = "Csig.Screensaver_Logo_URL_fb",
+	Csig_Screensaver_Video_URL_fb = "Csig.Screensaver_Video_URL_fb",
+	Csig_System_WiFi_IP_Address_fb = "Csig.System_WiFi_IP_Address_fb",
+	Csig_User_Groups_fb = "Csig.User_Groups_fb",
+
+	Csig_Voice_Capture_Result_fb = "Csig.Voice_Capture_Result_fb",
+}
 
 export default SerialState;
