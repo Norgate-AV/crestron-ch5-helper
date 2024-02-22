23d11574a1df891258f69b9a2f08644214a9ba69
Damien Butt <22627489+damienbutt@users.noreply.github.com>
2024-02-22 16:18:38 +0000

    test: start basic test file

--- tests/reserved.test.ts
+++ tests/reserved.test.ts
@@ -1,29 +1,20 @@
-import path from "node:path";
-import { fileURLToPath } from "node:url";
 import { describe, it, expect } from "vitest";
-import type CrestronCH5 from "../src/index.js";
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const object = (await import(
-    `${path.resolve(__dirname, "../dist/index.js")}`
-)) as typeof CrestronCH5;
-
-console.log(object);
+import CrestronCH5 from "../src/index.js";
 
 describe("Crestron CH5 Reserved Joins", () => {
     it("should import default export without error", () => {
-        expect(object).toBeDefined();
+        expect(CrestronCH5).toBeDefined();
     });
 
     it("should contain the correct values", () => {
         // Traverse the object and check for the correct values
-        expect(object.ReservedJoin.Analog.Event.Csig_All_Audio_Volume).toBe(
-            "Csig.All_Audio_Volume",
-        );
+        expect(
+            CrestronCH5.ReservedJoin.Analog.Event.Csig_All_Audio_Volume,
+        ).toBe("Csig.All_Audio_Volume");
 
-        expect(object.ReservedJoin.Analog.Event.Csig_Auto_Answer_Delay).toBe(
-            "Csig.Auto_Answer_Delay",
-        );
+        expect(
+            CrestronCH5.ReservedJoin.Analog.Event.Csig_Auto_Answer_Delay,
+        ).toBe("Csig.Auto_Answer_Delay");
     });
 
     it("should always pass", () => {
