84c0aac823ee1aef8aea3deed75b606edba798f2
Damien Butt <22627489+damienbutt@users.noreply.github.com>
2024-02-24 20:51:20 +0000

    feat(digital)!: update state reserved joins
    Removed:
    - Csig.Browser_kiosk_off_fb
    - Csig.Browser_kiosk_on_fb
    - Csig.Call_Active_fb
    - Csig.Hard_Button_1.Press
    - Csig.Hard_Button_2.Press
    - Csig.Hard_Button_3.Press
    - Csig.Hard_Button_4.Press
    - Csig.Hard_Button_5.Press
    - Csig.Lcd_Brt_Low_fb
    - Csig.License_Invalid_fb
    - Csig.License_Valid_fb
    - Csig.Trial_Period_Off_fb
    - Csig.Trial_Period_On_fb
    
    Added:
    + Csig.Auto_Frame_OFF_fb
    + Csig.Auto_Frame_ON_fb
    + Csig.Browser_kiosk_off_fb
    + Csig.Browser_kiosk_on_fb
    + Csig.Camera_Preset_0_fb
    + Csig.Camera_Preset_1_fb
    + Csig.Camera_Preset_2_fb
    + Csig.Camera_Preset_3_fb
    + Csig.Camera_Preset_4_fb
    + Csig.Camera_connected_fb
    + Csig.Capture_color_mode_auto_fb
    + Csig.Capture_color_mode_full_fb
    + Csig.Capture_color_mode_limited_fb
    + Csig.CallActive_fb
    + Csig.Conf_Spkr_Mute_Off_fb
    + Csig.Conf_Spkr_Mute_On_fb
    + Csig.Conf_Spkr_Mute_Toggle_fb
    + Csig.Default_Spkr_Mute_Off_fb
    + Csig.Default_Spkr_Mute_On_fb
    + Csig.Default_Spkr_Mute_Toggle_fb
    + Csig.Dock_Power_Status_fb
    + Csig.Hide_Project_fb
    + Csig.Huddly_Camera_Connected_fb
    + Csig.Image_Mode_Teams_fb
    + Csig.Image_Mode_Zoom_fb
    + Csig.Jabra_AF_Intelligent_fb
    + Csig.Jabra_AF_V_Director_fb
    + Csig.Lcd_Brt_Lo_fb
    + Csig.Logitech_fw_update_now_fb
    + Csig.Logitech_rightsight_disabled_fb
    + Csig.Logitech_rightsight_enabled_fb
    + Csig.Logitech_rightsight_toggle_fb
    + Csig.Logitech_sync_disabled_fb
    + Csig.Logitech_sync_enabled_fb
    + Csig.Logitech_sync_toggle_fb
    + Csig.MTR_APP_Exchange_signed_in_fb
    + Csig.MTR_APP_Teams_signed_in_fb
    + Csig.MTR_APP_in_meeting_fb
    + Csig.MTR_App_Signed_in_fb
    + Csig.Movement_Wakes_LCD_Disabled_fb
    + Csig.Movement_Wakes_LCD_Enabled_fb
    + Csig.Mtr_Coordinated_Meetings_fb
    + Csig.Occupancy_Sensor_Connected__fb
    + Csig.Occupancy_Sensor_Disconnected_fb
    + Csig.Occupancy_Status_Occupied_fb
    + Csig.Occupancy_Status_Vacant_fb
    + Csig.Panel_Occupancy_Occupied_fb
    + Csig.Panel_Occupancy_Vacant_fb
    + Csig.Polycom_TT_Disabled_fb
    + Csig.Polycom_TT_Enable_Group_Framing_fb
    + Csig.Polycom_TT_Enable_Talker_Framing__fb
    + Csig.Polycom_TT_High_Sensitivity_fb
    + Csig.Polycom_TT_Low_Sensitivity_fb
    + Csig.Polycom_TT_Medium_Margin__fb
    + Csig.Polycom_TT_Medium_Sensitivity_fb
    + Csig.Polycom_TT_Tight_Margin_fb
    + Csig.Polycom_TT_Wide_Margin__fb
    + Csig.Resume_from_Standby_fb
    + Csig.Sensor_Occupancy_Occupied_fb
    + Csig.Sensor_Occupancy_Vacant_fb
    + Csig.Show_Project_fb
    + Csig.Standby_fb
    + Csig.System_Battery_Health_Indicator_fb
    + Csig.UCENGINE_auto_zoom_speed_fast_fb
    + Csig.UCENGINE_auto_zoom_speed_fastest_fb
    + Csig.UCENGINE_auto_zoom_speed_medium_fb
    + Csig.UCENGINE_auto_zoom_speed_slow_fb
    + Csig.UCENGINE_auto_zoom_speed_slowest_fb
    + Csig.Windows_Update_Enable_ON_fb
    + Csig.Zoom_nightly_reboot_fb

--- src/ReservedJoin/Digital/State/index.ts
+++ src/ReservedJoin/Digital/State/index.ts
@@ -15,14 +15,17 @@ export const DigitalState = {
     Csig_Authentication_On_fb: "Csig.Authentication_On_fb",
     Csig_Auto_Answer_fb: "Csig.Auto_Answer_fb",
 
+    Csig_Auto_Frame_OFF_fb: "Csig.Auto_Frame_OFF_fb",
+    Csig_Auto_Frame_ON_fb: "Csig.Auto_Frame_ON_fb",
+
     Csig_Backlight_Off_fb: "Csig.Backlight_Off_fb",
     Csig_Backlight_On_fb: "Csig.Backlight_On_fb",
 
     Csig_BlueLedControl_fb: "Csig.BlueLedControl_fb",
     Csig_BlueStatusBarControl_fb: "Csig.BlueStatusBarControl_fb",
 
-    Csig_Browser_Kiosk_Off_fb: "Csig.Browser_kiosk_off_fb",
-    Csig_Browser_Kiosk_On_fb: "Csig.Browser_kiosk_on_fb",
+    Csig_Browser_kiosk_off_fb: "Csig.Browser_kiosk_off_fb",
+    Csig_Browser_kiosk_on_fb: "Csig.Browser_kiosk_on_fb",
     Csig_Busy_fb: "Csig.Busy_fb",
     Csig_Button_1_Off_fb: "Csig.Button_1_Off_fb",
     Csig_Button_2_Off_fb: "Csig.Button_2_Off_fb",
@@ -39,18 +42,39 @@ export const DigitalState = {
     Csig_Button_Toolbar_Disable_fb: "Csig.Button_Toolbar_Disable_fb",
     Csig_Button_Toolbar_Enable_fb: "Csig.Button_Toolbar_Enable_fb",
     Csig_Call_Terminated_fb: "Csig.Call_Terminated_fb",
-    Csig_Call_Active_fb: "Csig.Call_Active_fb",
+
+    Csig_Camera_Preset_0_fb: "Csig.Camera_Preset_0_fb",
+    Csig_Camera_Preset_1_fb: "Csig.Camera_Preset_1_fb",
+    Csig_Camera_Preset_2_fb: "Csig.Camera_Preset_2_fb",
+    Csig_Camera_Preset_3_fb: "Csig.Camera_Preset_3_fb",
+    Csig_Camera_Preset_4_fb: "Csig.Camera_Preset_4_fb",
+    Csig_Camera_connected_fb: "Csig.Camera_connected_fb",
+    Csig_Capture_color_mode_auto_fb: "Csig.Capture_color_mode_auto_fb",
+    Csig_Capture_color_mode_full_fb: "Csig.Capture_color_mode_full_fb",
+    Csig_Capture_color_mode_limited_fb: "Csig.Capture_color_mode_limited_fb",
+
+    Csig_CallActive_fb: "Csig.CallActive_fb",
 
     Csig_Close_Browser_fb: "Csig.Close_Browser_fb",
 
+    Csig_Conf_Spkr_Mute_Off_fb: "Csig.Conf_Spkr_Mute_Off_fb",
+    Csig_Conf_Spkr_Mute_On_fb: "Csig.Conf_Spkr_Mute_On_fb",
+    Csig_Conf_Spkr_Mute_Toggle_fb: "Csig.Conf_Spkr_Mute_Toggle_fb",
+
     Csig_ConnectedToServer_fb: "Csig.ConnectedToServer_fb",
     Csig_Control_Systems_Offline_fb: "Csig.Control_Systems_Offline_fb",
+
+    Csig_Default_Spkr_Mute_Off_fb: "Csig.Default_Spkr_Mute_Off_fb",
+    Csig_Default_Spkr_Mute_On_fb: "Csig.Default_Spkr_Mute_On_fb",
+    Csig_Default_Spkr_Mute_Toggle_fb: "Csig.Default_Spkr_Mute_Toggle_fb",
 
     Csig_Dialing_fb: "Csig.Dialing_fb",
     Csig_Disable_fb: "Csig.Disable_fb",
     Csig_DisableMulticast_fb: "Csig.DisableMulticast_fb",
     Csig_DisableStream_fb: "Csig.DisableStream_fb",
     Csig_Do_Not_Disturb_fb: "Csig.Do_Not_Disturb_fb",
+
+    Csig_Dock_Power_Status_fb: "Csig.Dock_Power_Status_fb",
 
     Csig_Enable_fb: "Csig.Enable_fb",
     Csig_EnableMulticast_fb: "Csig.EnableMulticast_fb",
@@ -69,19 +93,20 @@ export const DigitalState = {
     Csig_Hard_Button_3_On_fb: "Csig.Hard_Button_3_On_fb",
     Csig_Hard_Button_4_On_fb: "Csig.Hard_Button_4_On_fb",
     Csig_Hard_Button_5_On_fb: "Csig.Hard_Button_5_On_fb",
-    Csig_Hard_Button_1_Press: "Csig.Hard_Button_1.Press",
-    Csig_Hard_Button_2_Press: "Csig.Hard_Button_2.Press",
-    Csig_Hard_Button_3_Press: "Csig.Hard_Button_3.Press",
-    Csig_Hard_Button_4_Press: "Csig.Hard_Button_4.Press",
-    Csig_Hard_Button_5_Press: "Csig.Hard_Button_5.Press",
     Csig_Hard_Button_Brt_Auto_Off_fb: "Csig.Hard_Button_Brt_Auto_Off_fb",
     Csig_Hard_Button_Brt_Auto_On_fb: "Csig.Hard_Button_Brt_Auto_On_fb",
 
     Csig_Hide_Button_Toolbar_fb: "Csig.Hide_Button_Toolbar_fb",
     Csig_Hide_Opened_App_fb: "Csig.Hide_Opened_App_fb",
+    Csig_Hide_Project_fb: "Csig.Hide_Project_fb",
     Csig_Hold_fb: "Csig.Hold_fb",
+    Csig_Huddly_Camera_Connected_fb: "Csig.Huddly_Camera_Connected_fb",
+    Csig_Image_Mode_Teams_fb: "Csig.Image_Mode_Teams_fb",
+    Csig_Image_Mode_Zoom_fb: "Csig.Image_Mode_Zoom_fb",
     Csig_In_Progress_fb: "Csig.In_Progress_fb",
     Csig_Incoming_fb: "Csig.Incoming_fb",
+    Csig_Jabra_AF_Intelligent_fb: "Csig.Jabra_AF_Intelligent_fb",
+    Csig_Jabra_AF_V_Director_fb: "Csig.Jabra_AF_V_Director_fb",
     Csig_Key_Backlight_High_Level_fb: "Csig.Key_Backlight_High_Level_fb",
     Csig_Key_Backlight_Low_Level_fb: "Csig.Key_Backlight_Low_Level_fb",
 
@@ -95,20 +120,36 @@ export const DigitalState = {
     Csig_Lcd_Brt_Auto_On_fb: "Csig.Lcd_Brt_Auto_On_fb",
 
     Csig_Lcd_Brt_High_fb: "Csig.Lcd_Brt_High_fb",
-
-    Csig_Lcd_Brt_Low_fb: "Csig.Lcd_Brt_Low_fb",
-
+    Csig_Lcd_Brt_Lo_fb: "Csig.Lcd_Brt_Lo_fb",
     Csig_Lcd_Brt_Med_fb: "Csig.Lcd_Brt_Med_fb",
 
     Csig_LCD_Day_Mode_fb: "Csig.LCD_Day_Mode_fb",
     Csig_LCD_Night_Mode_fb: "Csig.LCD_Night_Mode_fb",
     Csig_LedAccessoryConnected_fb: "Csig.LedAccessoryConnected_fb",
-    Csig_License_Invalid_fb: "Csig.License_Invalid_fb",
-    Csig_License_Valid_fb: "Csig.License_Valid_fb",
+    Csig_Logitech_fw_update_now_fb: "Csig.Logitech_fw_update_now_fb",
+    Csig_Logitech_rightsight_disabled_fb:
+        "Csig.Logitech_rightsight_disabled_fb",
+    Csig_Logitech_rightsight_enabled_fb: "Csig.Logitech_rightsight_enabled_fb",
+    Csig_Logitech_rightsight_toggle_fb: "Csig.Logitech_rightsight_toggle_fb",
+    Csig_Logitech_sync_disabled_fb: "Csig.Logitech_sync_disabled_fb",
+    Csig_Logitech_sync_enabled_fb: "Csig.Logitech_sync_enabled_fb",
+    Csig_Logitech_sync_toggle_fb: "Csig.Logitech_sync_toggle_fb",
+    Csig_MTR_APP_Exchange_signed_in_fb: "Csig.MTR_APP_Exchange_signed_in_fb",
+    Csig_MTR_APP_Teams_signed_in_fb: "Csig.MTR_APP_Teams_signed_in_fb",
+    Csig_MTR_APP_in_meeting_fb: "Csig.MTR_APP_in_meeting_fb",
+    Csig_MTR_App_Signed_in_fb: "Csig.MTR_App_Signed_in_fb",
+    Csig_Movement_Wakes_LCD_Disabled_fb: "Csig.Movement_Wakes_LCD_Disabled_fb",
+    Csig_Movement_Wakes_LCD_Enabled_fb: "Csig.Movement_Wakes_LCD_Enabled_fb",
+    Csig_Mtr_Coordinated_Meetings_fb: "Csig.Mtr_Coordinated_Meetings_fb",
 
     Csig_Muted_fb: "Csig.Muted_fb",
     Csig_Not_Admin_fb: "Csig.Not_Admin_fb",
     Csig_Not_Authenticated_fb: "Csig.Not_Authenticated_fb",
+    Csig_Occupancy_Sensor_Connected__fb: "Csig.Occupancy_Sensor_Connected__fb",
+    Csig_Occupancy_Sensor_Disconnected_fb:
+        "Csig.Occupancy_Sensor_Disconnected_fb",
+    Csig_Occupancy_Status_Occupied_fb: "Csig.Occupancy_Status_Occupied_fb",
+    Csig_Occupancy_Status_Vacant_fb: "Csig.Occupancy_Status_Vacant_fb",
     Csig_Open_App_fb: "Csig.Open_App_fb",
     Csig_Open_Browser_fb: "Csig.Open_Browser_fb",
 
@@ -118,8 +159,24 @@ export const DigitalState = {
     Csig_PTT_fb: "Csig.PTT_fb",
     Csig_PTT_mode_fb: "Csig.PTT_mode_fb",
 
+    Csig_Panel_Occupancy_Occupied_fb: "Csig.Panel_Occupancy_Occupied_fb",
+    Csig_Panel_Occupancy_Vacant_fb: "Csig.Panel_Occupancy_Vacant_fb",
+    Csig_Polycom_TT_Disabled_fb: "Csig.Polycom_TT_Disabled_fb",
+    Csig_Polycom_TT_Enable_Group_Framing_fb:
+        "Csig.Polycom_TT_Enable_Group_Framing_fb",
+    Csig_Polycom_TT_Enable_Talker_Framing__fb:
+        "Csig.Polycom_TT_Enable_Talker_Framing__fb",
+    Csig_Polycom_TT_High_Sensitivity_fb: "Csig.Polycom_TT_High_Sensitivity_fb",
+    Csig_Polycom_TT_Low_Sensitivity_fb: "Csig.Polycom_TT_Low_Sensitivity_fb",
+    Csig_Polycom_TT_Medium_Margin__fb: "Csig.Polycom_TT_Medium_Margin__fb",
+    Csig_Polycom_TT_Medium_Sensitivity_fb:
+        "Csig.Polycom_TT_Medium_Sensitivity_fb",
+    Csig_Polycom_TT_Tight_Margin_fb: "Csig.Polycom_TT_Tight_Margin_fb",
+    Csig_Polycom_TT_Wide_Margin__fb: "Csig.Polycom_TT_Wide_Margin__fb",
+
     Csig_RedLedControl_fb: "Csig.RedLedControl_fb",
     Csig_RedStatusBarControl_fb: "Csig.RedStatusBarControl_fb",
+    Csig_Resume_from_Standby_fb: "Csig.Resume_from_Standby_fb",
 
     Csig_RF_Reader_Connected_fb: "Csig.RF_Reader_Connected_fb",
     Csig_RF_Reader_Disconnected_fb: "Csig.RF_Reader_Disconnected_fb",
@@ -133,11 +190,17 @@ export const DigitalState = {
         "Csig.Screensaver_Indoor_Temperature_Enable_fb",
     Csig_Screensaver_off_fb: "Csig.Screensaver_off_fb",
     Csig_Screensaver_on_fb: "Csig.Screensaver_on_fb",
+    Csig_Sensor_Occupancy_Occupied_fb: "Csig.Sensor_Occupancy_Occupied_fb",
+    Csig_Sensor_Occupancy_Vacant_fb: "Csig.Sensor_Occupancy_Vacant_fb",
     Csig_Screensaver_Outdoor_Temperature_Disable_fb:
         "Csig.Screensaver_Outdoor_Temperature_Disable_fb",
     Csig_Screensaver_Outdoor_Temperature_Enable_fb:
         "Csig.Screensaver_Outdoor_Temperature_Enable_fb",
     Csig_Show_Button_Toolbar_fb: "Csig.Show_Button_Toolbar_fb",
+    Csig_Show_Project_fb: "Csig.Show_Project_fb",
+    Csig_Standby_fb: "Csig.Standby_fb",
+    Csig_System_Battery_Health_Indicator_fb:
+        "Csig.System_Battery_Health_Indicator_fb",
 
     Csig_System_Media_Mute_fb: "Csig.System_Media_Mute_fb",
     Csig_System_Media_Unmute_fb: "Csig.System_Media_Unmute_fb",
@@ -145,8 +208,17 @@ export const DigitalState = {
     Csig_Temp_Tenths_Off_fb: "Csig.Temp_Tenths_Off_fb",
     Csig_Temp_Tenths_On_fb: "Csig.Temp_Tenths_On_fb",
 
-    Csig_Trial_Period_Off_fb: "Csig.Trial_Period_Off_fb",
-    Csig_Trial_Period_On_fb: "Csig.Trial_Period_On_fb",
+    Csig_UCENGINE_auto_zoom_speed_fast_fb:
+        "Csig.UCENGINE_auto_zoom_speed_fast_fb",
+    Csig_UCENGINE_auto_zoom_speed_fastest_fb:
+        "Csig.UCENGINE_auto_zoom_speed_fastest_fb",
+    Csig_UCENGINE_auto_zoom_speed_medium_fb:
+        "Csig.UCENGINE_auto_zoom_speed_medium_fb",
+    Csig_UCENGINE_auto_zoom_speed_slow_fb:
+        "Csig.UCENGINE_auto_zoom_speed_slow_fb",
+    Csig_UCENGINE_auto_zoom_speed_slowest_fb:
+        "Csig.UCENGINE_auto_zoom_speed_slowest_fb",
+
     Csig_Voice_Capture_Complete_fb: "Csig.Voice_Capture_Complete_fb",
     Csig_Voice_Capture_Decoding_fb: "Csig.Voice_Capture_Decoding_fb",
     Csig_Voice_Capture_Error_fb: "Csig.Voice_Capture_Error_fb",
@@ -154,6 +226,8 @@ export const DigitalState = {
     Csig_Voice_Capture_Recording_fb: "Csig.Voice_Capture_Recording_fb",
     Csig_Voice_Recognition_Service_Online_fb:
         "Csig.Voice_Recognition_Service_Online_fb",
+    Csig_Windows_Update_Enable_ON_fb: "Csig.Windows_Update_Enable_ON_fb",
+    Csig_Zoom_nightly_reboot_fb: "Csig.Zoom_nightly_reboot_fb",
 } as const;
 
 export type DigitalState = ObjectTypes<typeof DigitalState>;
